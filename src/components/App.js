d7c5200c237e261ded962a16a311bfa7708d9226
andytrandaoanh <andytrandaoanh@gmail.com>
2019-07-29 07:35:30 +0700

    layout work

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ToolbarAbstraction from './appbars/CookBookB31';
 
 
 function App () {
 	return (
 		<div className = "App">
-			<h1>Hello World!</h1>        
+			<ToolbarAbstraction />      
     	</div>
 	)
 }
---
+++ src/components/grids/CookBookA43.js
@@ -0,0 +1,54 @@
+//FIXED COLUMN LAYOUT
+//Mixed values
+//set container to 12 (100%) and the  xs value to 6 (50%)
+
+
+import React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+});
+
+const FixedColumnLayout = withStyles(styles)(({ classes, width }) => (
+<div className={classes.root}>
+  <Grid container spacing={4}>
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>xs=12</Paper>
+    </Grid>
+    <Grid item xs={6}>
+      <Paper className={classes.paper}>xs=6</Paper>
+    </Grid>
+    <Grid item xs={6}>
+      <Paper className={classes.paper}>xs=6</Paper>
+    </Grid>
+    <Grid item xs={6}>
+      <Paper className={classes.paper}>xs=6</Paper>
+    </Grid>
+    <Grid item xs={6}>
+      <Paper className={classes.paper}>xs=6</Paper>
+    </Grid>
+    <Grid item xs={6}>
+      <Paper className={classes.paper}>xs=6</Paper>
+    </Grid>
+    <Grid item xs={6}>
+      <Paper className={classes.paper}>xs=6</Paper>
+    </Grid>
+    <Grid item xs={12}>
+      <Paper className={classes.paper}>xs=12</Paper>
+    </Grid>
+  </Grid>
+</div>
+));
+
+export default FixedColumnLayout;
